188bd1e910ec1addde9f5d52190a08d5515901f1
Hai1906 <haileds9339@gmail.com>
2024-07-22 16:42:32 +0700

    up

---
+++ YAHALLO.Domain/Common/Interfaces/Socket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YAHALLO.Domain.Enums.Socket;
+
+namespace YAHALLO.Domain.Common.Interfaces
+{
+    public class SocketServerInfo
+    {
+        public SocketServerInfo() { }
+        public SocketServerInfo(string servername, List<string> hosts, int port)
+        {
+            ServerName = servername;
+            Hosts = hosts;  
+            Port = port;
+        }
+        public string ServerName { get; set; } = null!;
+        public List<string> Hosts { get; set; } = null!;
+        public int Port { get; set; }
+    }
+    public class SocketQueue
+    {
+        public SocketQueue() { }    
+        public SocketQueue(SocketEnums type, byte[]? data) 
+        {
+            Type = type;
+            Data = data;    
+        }
+        public SocketEnums Type { get; set; }
+        public byte[]? Data { get; set; }
+    }
+}
