083974dccfc606cdf926fef1be60eddf08dbd313
Hai1906 <haileds9339@gmail.com>
2024-07-26 16:53:00 +0700

    objects {AttechmentEntity,CountingEntitity,MetadataEntity}

--- YAHALLO.Infrastructure/Data/Configurations/BlogConfiguration.cs
+++ YAHALLO.Infrastructure/Data/Configurations/BlogConfiguration.cs
@@ -14,6 +14,7 @@ namespace YAHALLO.Infrastructure.Data.Configurations
         public void Configure(EntityTypeBuilder<BlogEntity> builder)
         {
             builder.HasKey(x=> x.Id);
+            
 
             builder.HasMany(x => x.DisLikeMetadataByDate)
                 .WithOne()
