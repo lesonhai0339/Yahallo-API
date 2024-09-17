92b781863858549d07b7b41a3e1c2841b2361a4a
Hai1906 <haileds9339@gmail.com>
2024-09-17 16:50:24 +0700

    up

---
+++ YAHALLO.Application/Commands/BlogCommand/Restore/RestoreBlogCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAHALLO.Application.Commands.BlogCommand.Restore
+{
+    public class RestoreBlogCommandValidator: AbstractValidator<RestoreBlogCommand> 
+    {
+        public RestoreBlogCommandValidator() 
+        {
+            RuleFor(x=> x.BlogId).NotEmpty().NotNull().WithMessage("Blog Id does not empty");
+        }
+    }
+}
