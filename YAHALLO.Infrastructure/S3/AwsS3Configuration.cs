7818bd42f1db98132df711c0871fcc74598f0dbe
Yahallo Dev <haileds939@gmail.com>
2026-06-01 14:50:54 +0700

    up

---
+++ YAHALLO.Infrastructure/S3/AwsS3Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YAHALLO.Domain.Repositories.Storage;
+
+namespace YAHALLO.Infrastructure.S3
+{
+    internal class AwsS3Configuration
+    {
+    }
+}
---
+++ YAHALLO.Infrastructure/S3/AwsS3Service.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Threading.Tasks;
+using YAHALLO.Domain.Repositories.Storage;
+
+namespace YAHALLO.Infrastructure.S3
+{
+    internal class AwsS3Service: IStorageService
+    {
+    }
+}
